b91c5247eb6b3fd40bae4ed2f0095790e738c5ce
Sungmin Cho <sc107@illinois.edu>
2026-03-02 22:43:16 +0900

    refactor: change pricing to defaultPricing

--- backend/src/routes/simulate.ts
+++ backend/src/routes/simulate.ts
@@ -2,14 +2,14 @@ import { FastifyInstance } from "fastify";
 import { calculateCost } from "../services/cost/totalCost";
 import { SimulationInput } from "../services/cost/totalCost";
 import { simulationSchema } from "../schemas/simulation.schema";
-import { pricing } from "../data/pricingData";
+import { defaultPricing } from "../data/pricingData";
 
 export default async function simulateRoute(app: FastifyInstance) {
     app.post("/simulate", 
         { schema: simulationSchema } , 
         async (request) => {
         const body = request.body as SimulationInput;
-        const totalCost = calculateCost(body, pricing);
+        const totalCost = calculateCost(body, defaultPricing);
         return { totalCost };
         }
     ); 
