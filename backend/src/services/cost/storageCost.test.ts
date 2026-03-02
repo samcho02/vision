c626a45ac571fc0c150dadb78ae26b4ce0a744ca
Sungmin Cho <sc107@illinois.edu>
2026-03-02 23:23:59 +0900

    refactor: rewrite unit test using interface

--- backend/src/services/cost/storageCost.test.ts
+++ backend/src/services/cost/storageCost.test.ts
@@ -5,31 +5,30 @@ import { StoragePricing } from "../../data/pricingData";
 
 
 describe("storageCost Test", () => {
+    const baseInput: SimulationInput = {
+        computeRequests: 0,
+        avgExecutionMs: 0,
+        storageGB: 0,
+        outboundGB: 0,
+        dbReads: 0,
+        dbWrites: 0,
+    };
 
+    const basePricing: StoragePricing = {
+        perGBMonth: 0
+    }; 
 
     it("Test 1: Returns 0 when no usage", () => {
-        const input = {
-            storageGB: 0
-        };
-    
-        const pricing = {
-            perGBMonth: 0
-        };
-
-        const result = storageCost(input as SimulationInput, 
-            pricing as StoragePricing);
+        const result = storageCost(baseInput, basePricing);
         expect(result).toBe(0);
     });
 
     it("Test 2: Returns request cost correctly ", () => {
-        const input = {
-            storageGB: 2
-        };
-        const pricing = { 
-            perGBMonth: 3
-        }
-        const result = storageCost(input as SimulationInput,
-            pricing as StoragePricing);
+        const input = { ...baseInput, storageGB: 2 };
+        const pricing = { ...basePricing, perGBMonth: 3 }
+        
+        const result = storageCost(input, pricing);
+        
         expect(result).toBe(6);
     });
 });
